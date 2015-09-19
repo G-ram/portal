8fc0d9285d5d64beec972797b865a6fac6f255a6
G-Ram <ggobieski@yahoo.com>
2015-09-19 14:21:31 -0400

    working base sensor code

---
+++ python/main.py
@@ -0,0 +1,12 @@
+import pyopencl
+import argparse
+
+from sensor import Sensor
+
+def main():
+    hey = 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Raster to SVG converter')
+    print "Starting..."
+    main()
