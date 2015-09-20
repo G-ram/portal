68fac12adcc9af48b9cf2f57795a886721d48bc0
G-Ram <ggobieski@yahoo.com>
2015-09-19 23:34:42 -0400

    done py touch

--- python/main.py
+++ python/main.py
@@ -4,14 +4,18 @@ import argparse
 import time
 import scipy as sp
 import scipy.ndimage
+import requests
+import json
 
 from sensor import Sensor
+
+root_url = "http://localhost:3000"
 
 def normalize(a, min, max):
     if(max == min):
         max = 0.5;
         min = 0.0;
-    return float(a - min)/float(max - min)
+    return int((float(a - min)/float(max - min)) * 255.0)
 
 def main():
     global sensor
@@ -23,7 +27,8 @@ def main():
         raise
         return
 
-    cv.namedWindow("ImageWindow", cv.WINDOW_AUTOSIZE);
+    #cv.namedWindow("Threshold", cv.WINDOW_AUTOSIZE);
+    cv.namedWindow("Contours", cv.WINDOW_AUTOSIZE);
     nfunc = np.vectorize(normalize)
     images = get_next_image()
     baseline_frame = images[-1]
@@ -39,14 +44,46 @@ def main():
         max = np.amax(pixels_zeroed)
         pixels_normalized = nfunc(pixels_zeroed, min, max)
         large = sp.ndimage.zoom(pixels_normalized, 10, order=1)
-        detector = cv.SimpleBlobDetector()
-        keypoints = detector.detect(large)
-        im_with_keypoints = cv.drawKeypoints(large, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv.imshow("ImageWindow",im_with_keypoints);
+        large = large.astype(np.uint8);
+        large = cv.medianBlur(large,5)
+        #large = cv.GaussianBlur(large,(7,7),0)
+        #stuff, blobs = cv.threshold(large,150,255,cv.THRESH_BINARY)
+        stuff, blobs = cv.threshold(large,170,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+        contours, hierarchy = cv.findContours(blobs, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        out = np.zeros((height*10,width*10,3), np.uint8)
+        cv.drawContours(out,contours,-1,(255,255,0),3)
+
+        regions_found = len(contours)
+
+        contours = np.vstack(contours).squeeze()
+        rect = cv.minAreaRect(contours)
+        box = cv.cv.BoxPoints(rect)
+        box = np.int0(box)
+        cv.drawContours(out,[box],0,(0,255,255),2)
+
+        #cv.imshow("Threshold", blobs);
+        cv.imshow("Contours", out)
         cv.waitKey(1)
+
+        x = rect[0][0]
+        y = rect[0][1]
+        angle = rect[2];
+
+        send(x, y, angle)
+
+        time.sleep(0.2)
 
     sensor.close()
     print "Done. Everything cleaned up."
+
+def send(x, y, theta):
+    root_url = "hey"
+    payload = {'x': x, 'y': y, 'angle': theta}
+    #r = requests.post(root_url+"/data/", data=payload)
+    #if r.json()['code'] == 200:
+    #	print "Sent."
+    #else:
+    #	print r.json()
 
 def get_next_image():
     images = sensor.getAllImages()
